3726ae19d6dc70a50c1d2613378907284781644d
Aaron T <aaron.taulaaut@gmail.com>
2025-08-06 11:42:11 +1200

    feat: migrate from Vue to React, update components and styles, and add treatments and case studies data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Main React App component
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Oral Health App</h1>
+      {/* ...routes and components... */}
+    </div>
+  );
+}
+
+export default App;
